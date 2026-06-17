19c9b290df97d28a90572a54b0b5c7b04fd9e64f
jimlorm <jimlormmartinez@gmail.com>
2026-06-17 16:51:51 +0800

    Added error-handling functions

--- tokenizer.h
+++ tokenizer.h
@@ -60,3 +60,92 @@ void tokenize(const char* expression, Queue* tokenQueue, ErrorStatus* status)
         }
     }
 }
+
+void hasMismatchedParenthesis(Queue* infix, ErrorStatus* status)
+{
+    *status = SUCCESS;
+    Stack* stack = createStack();
+    Queue* infixCopy = copyQueue(infix);
+    Token t;
+
+    // Added the status check so it stops early if an error is found
+    while (!isEmptyQueue(infixCopy) && *status == SUCCESS) 
+    {
+        t = dequeue(infixCopy);
+
+        if (t.type == PARENTHESIS) 
+        {
+            if (t.data.symbol == '(') 
+            {
+                push(stack, t);
+            }
+            else if (t.data.symbol == ')') 
+            {
+                if (!isEmptyStack(stack)) 
+                {
+                    pop(stack);
+                }
+                else 
+                {
+                    *status = ERR_MISMATCH_PAREN; 
+                }
+            }
+        }
+    }
+
+    if (*status == SUCCESS && !isEmptyStack(stack)) 
+        *status = ERR_MISMATCH_PAREN;
+
+    deleteStack(&stack);
+    deleteQueue(&infixCopy);
+}
+
+void isMalformedExpression(Queue* infix, ErrorStatus* status)
+{
+    *status = SUCCESS;
+    Queue* infixCopy = copyQueue(infix);
+    bool expectOperand = true; 
+
+    while (!isEmptyQueue(infixCopy) && *status == SUCCESS) 
+    {
+        Token t = dequeue(infixCopy);
+
+        if (t.type == OPERAND) 
+        {
+            expectOperand = false; 
+        }
+        else if (t.type == OPERATOR) 
+        {
+            if (expectOperand) 
+            {
+                *status = ERR_MALFORMED; 
+            }
+            else 
+            {
+                expectOperand = true; 
+            }
+        }
+        else if (t.type == PARENTHESIS) 
+        {
+            if (t.data.symbol == '(') 
+            {
+                expectOperand = true;
+            } 
+            else if (t.data.symbol == ')') 
+            {
+                if (expectOperand) 
+                {
+                    *status = ERR_MALFORMED;
+                }
+                expectOperand = false; 
+            }
+        }
+    }
+
+    if (*status == SUCCESS && expectOperand) 
+    {
+        *status = ERR_MALFORMED;
+    }
+
+    deleteQueue(&infixCopy);
+}
