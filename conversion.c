6d73a8b3b9e6bfc2e0b84973199fe6ff5f61f34a
jimlorm <jimlormmartinez@gmail.com>
2026-06-17 17:00:05 +0800

    Initial skeleton file

---
+++ conversion.c
@@ -0,0 +1,19 @@
+#ifndef CONVERTER_H
+#define CONVERTER_H
+
+#include "stacks.h"
+#include "queue.h"
+
+void reverseQueue(Queue* q)
+{
+}
+
+int precedenceLevel(char op)
+{
+}
+
+void infixToPrefix(Queue* infixQueue, Queue* prefixQueue, ErrorStatus* status)
+{
+}
+
+#endif // CONVERTER_H
